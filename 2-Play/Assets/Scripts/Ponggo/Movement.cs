03a26f6acfa44ed83257a502d673c69f608e642b
Eucleth <eucleth.contreras@gmail.com>
2022-01-01 18:09:58 +0100

    Updated some games for better functionality

--- 2-Play/Assets/Scripts/Ponggo/Movement.cs
+++ 2-Play/Assets/Scripts/Ponggo/Movement.cs
@@ -14,21 +14,6 @@ public class Movement : MonoBehaviour
         rb.velocity = new Vector2(joystick.Horizontal * speed, joystick.Vertical * speed);
     }
 
-    /* usato su Pong
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.tag == "PowerBigger")
-        {
-            Vector3 temp = this.transform.localScale;
-            temp.x += 2.0f;
-            this.transform.localScale = temp;
-        }
-        if(other.tag == "PowerFaster")
-        {
-            this.speed += 5.0f;
-        }
-    } */
-
     public float GetSpeed()
     {
         return this.speed;
